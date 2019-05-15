17818e748e9044b453d8d525b73a0abee3bf675c
Julien Moreau <pixeye@pixeye.net>
2019-05-15 15:54:25 +0200

    simplify, use float left & autofocus

--- htdocs/base64.php
+++ htdocs/base64.php
@@ -1,9 +1,7 @@
 <?php
 /**
  * Last commit of this file (GMT):
- * $Id$
- *
- * PHP version 5.3
+ * $Id: 849e5f33f21126672c44669bc25309893ac6b5b2 $
  *
  * @category  PHP
  * @package   PhpTests
@@ -17,22 +15,32 @@
  */
 
 $charset = 'UTF-8';
+$title = 'Base64 encode/decode';
 file_exists('Include/head.php') && require_once 'Include/head.php';
 $source = isSet($_REQUEST['source']) ? trim($_REQUEST['source']) : '';
 $srcLen = strlen($source);
+$option = $srcLen? '': ' autofocus';
 ?>
 	<p><br/></p>
-  <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+	<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
 	  <div class="center">
+	    <div class="btn-grp">
+	      <input type="reset"  value="Reset"  class="btn"/>
+	      <input type="submit" value="Encode" class="btn btn-info"    name="encode"/>
+	      <input type="submit" value="Decode" class="btn btn-primary" name="decode"/>
+	    </div><br/>
+	  </div>
+
+	  <div style="float:left">
 	    <label>Source string to encode or decode:<br/>
-        <textarea cols="100" rows="15" name="source"><?php echo $source?></textarea>
-	    </label><br/><br/>
-	    <input type="reset" value="Reset"/>
-	    <input type="submit" value="Encode" name="encode"/>
-	    <input type="submit" value="Decode" name="decode"/>
+	    <textarea<?php echo $option?> cols="110" rows="15" name="source"><?php echo $source?></textarea>
+	    </label><br/>
 <?php
-if ($srcLen>0) {
-    echo "\t    <p>This string is $srcLen characters long.</p>\n\n";
+if ($srcLen<=0) {
+	echo "\t  </div>\n";
+} else {
+    echo "\t    <p>This string is $srcLen characters long.</p>\n";
+    echo "\t  </div>\n";
     // echo "<pre>_REQUEST = "; print_r($_REQUEST); echo "</pre>\n";
     if (isSet($_REQUEST['encode'])) {
         $type = 'Encoded';
@@ -47,20 +55,12 @@ if ($srcLen>0) {
         $result = htmlSpecialChars($result);
     }
     // $result = nl2br($result);
-    echo "\t    <p><b>$type</b> string is:</p>\n\n";
-    echo "\t    <textarea cols=\"100\" rows=\"15\" name=\"result\">";
-    echo "$result</textarea>\n\n";
-    echo "\t    <p>This string is $resLen characters long.";
-    if ($resLen==16) {
-        if ($result==md5('toto', true)) {
-            echo "<br/>\n\t     Match found! :)";
-        }
-        echo "</p>\n";
-        $Tmp = unpack('H*', $result);
-        echo "<pre>unpack('H*', $result) = "; var_dump($Tmp); echo "</pre>\n";
-    } else {
-        echo "</p>\n";
-    }
+    echo "\t  <div style=\"float:left\">\n";
+    echo "\t    <label>$type string is:<br/>\n\n";
+    echo "\t    <textarea autofocus cols=\"110\" rows=\"15\" name=\"result\">";
+    echo "$result</textarea></label><br style=\"clear:both\"/>\n\n";
+
+    echo "\t    <p>This string is $resLen characters long.</p>\n";
 }
 ?>
 	  </div>
